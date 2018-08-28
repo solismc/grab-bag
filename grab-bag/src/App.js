f79f80ac0d3f7d684f23354c57efa01619201686
Manuel C Solis <manuelsolis@Manuels-MacBook-Air.local>
2018-08-28 09:59:03 -0400

    Input fields created

--- grab-bag/src/App.js
+++ grab-bag/src/App.js
@@ -8,11 +8,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Team Name Raffle</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <label for="example">Enter your team name</label>
+        <input id="example" type="text" name="text"></input>
+        <button>Submit</button>
+        <p className="App-intro"></p>
       </div>
     );
   }
---
+++ grab-bag/src/Botton.js
@@ -0,0 +1,31 @@
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+
+
+constructor(props) {
+    super(props);
+    this.setState() 
+        teamName = []
+    }
+}:
+
+
+class Button extends Component {
+    render() {
+        console.log('This is the name of the team you entered', teamName)
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+
+Button.teamName = teamName;
+
+
+export default Button;
