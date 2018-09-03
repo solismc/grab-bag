67a58d37185b2ae01758ddcae68d41afe1085ae3
Manuel C Solis <manuelsolis@Manuels-MacBook-Air.local>
2018-09-03 18:07:01 -0400

    Created data json file

---
+++ disc-profiles/src/CategoryLists.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class personality extends Component {
+    render() {
+
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default personality;
